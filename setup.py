0772168395b7e50c90161a3a98d8c969278af0a4
Vitor Leonardo Pauloski <73397198+vitorpauloski@users.noreply.github.com>
2021-02-04 11:53:53 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding = 'utf-8') as fh:
 
 setuptools.setup(
     name = 'GuruStock',
-    version = '1.0.0',
+    version = '1.0.1',
     author = 'Vitor Leonardo Pauloski',
     author_email = 'vitorleonardo.pauloski@gmail.com',
     description = "GuruStock is a Python package for extracting market data from Guru app.",
